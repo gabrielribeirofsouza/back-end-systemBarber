a9440ed2327b1668ab55b6a787a3a4b0f871b76c
unknown <jovem12345@gmail.com>
2025-11-16 03:30:59 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ app.use('/api/horarios', horariosRoutes);
 
 app.get('/api/health', (req, res) => res.json({ ok: true }));
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 init().then(() => {
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
